f4e6be24a333d90a4f97ea44fca7c506223561c1
Iliya Sumkov <47688469+Trunker2010@users.noreply.github.com>
2021-07-29 11:03:31 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int number = 25;
         //напишите тут ваш код
-        System.out.println(25*25);
+        System.out.println(number*number);
     }
 }
